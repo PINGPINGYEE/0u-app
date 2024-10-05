874a53a8311f12d12479ac1c4d37093ef89a9205
PINGPINGYEE <owb7994@nate.com>
2024-10-06 04:01:40 +0900

    Feat: 기본 설정

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { FaTree } from 'react-icons/fa'; // 나무 모양 아이콘 사용
 import '../HomePage.css';
 import '../App.css';
 
 function HomePage() {
   return (
     <div className="homepage">
+      <div className="intro-banner">
+        <FaTree className="tree-icon" /> {/* 나무 모양 아이콘 */}
+        <p className="intro-description" style={{ color:'black' }}>
+          오늘 <span className="highlight">3000명</span> 사람들이 동참했어요.
+        </p>
+      </div>
       <div className="search-section">
         <input
           type="text"
@@ -12,11 +19,6 @@ function HomePage() {
           placeholder="검색어를 입력하세요..."
         />
         <button className="search-button">Search</button>
-      </div>
-      <div className="hero-section">
-        <h1>0U - 공유와 재사용을 통한 지속 가능한 생활</h1>
-        <p>불필요한 자원을 공유하고 환경을 보호하세요. 0U와 함께 더 나은 지구를 만듭시다.</p>
-        <button className="cta-button">지금 시작하기</button>
       </div>
     </div>
   );
